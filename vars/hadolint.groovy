2ad3cdcac9941da78bd09a4a16acbfc4ba3d51fe
Aswin1118 <aswinvj390@gmail.com>
2023-08-30 15:17:32 +0530

    updated hadolint script

--- vars/hadolint.groovy
+++ vars/hadolint.groovy
@@ -2,11 +2,4 @@ def call() {
     def dockerfilePath = 'Dockerfile'
     def hadolintCommand = "hadolint ${dockerfilePath}"
     def dockerfileExists = fileExists(dockerfilePath)
-    
-    def hadolintOutput = hadolintCommand.execute().text
-
-    println("Hadolint Output:")
-    println(hadolintOutput)
-
-    println("Dockerfile Exists: ${dockerfileExists}")
 }
