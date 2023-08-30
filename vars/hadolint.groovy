fcb9fa1f6d040d180fc9fd0674b5433e05db156d
Aswin1118 <aswinvj390@gmail.com>
2023-08-30 15:22:21 +0530

    updated script

--- vars/hadolint.groovy
+++ vars/hadolint.groovy
@@ -2,4 +2,17 @@ def call() {
     def dockerfilePath = 'Dockerfile'
     def hadolintCommand = "hadolint ${dockerfilePath}"
     def dockerfileExists = fileExists(dockerfilePath)
+
+    if (dockerfileExists) {
+        def lintProcess = hadolintCommand.execute()
+        lintProcess.waitFor()
+
+        if (lintProcess.exitValue() != 0) {
+            throw new RuntimeException("Lint errors found in the Dockerfile")
+        } else {
+            println("No lint errors found.")
+        }
+    } else {
+        throw new RuntimeException("Dockerfile not found.")
+    }
 }
