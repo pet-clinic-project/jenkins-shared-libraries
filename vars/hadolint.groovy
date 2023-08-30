a5581a432714a18af216bee386e51fb4d7e3993e
Aswin1118 <aswinvj390@gmail.com>
2023-08-30 15:47:36 +0530

    updated

--- vars/hadolint.groovy
+++ vars/hadolint.groovy
@@ -1,6 +1,17 @@
 def call() {
     def dockerfilePath = 'Dockerfile'
     def hadolintCommand = "hadolint ${dockerfilePath}"
-    def dockerfileExists = fileExists(dockerfilePath)
+    def cmd = "docker run --rm -i hadolint/hadolint:v2.7.0 < ${dockerfilePath}"
+    
+    def proc = cmd.execute()
+    proc.waitForOrKill(10000)
+    
+    def exitValue = proc.exitValue()
+    
+    if (exitValue == 0) {
+        echo "Dockerfile passed hadolint checks."
+    } else {
+        error "Dockerfile failed hadolint checks."
+    }
 
 }
