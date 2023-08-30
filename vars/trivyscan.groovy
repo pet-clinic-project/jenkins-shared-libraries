60ea8c2129570e3e95dc0d5150b1e85afa3b269a
Aswin1118 <aswinvj390@gmail.com>
2023-08-30 12:56:17 +0530

    removed

--- vars/trivyscan.groovy
+++
@@ -1,7 +0,0 @@
-def call(String imageNameAndTag) {
-    def command = "sudo trivy image ${imageNameAndTag}"
-    def trivyOutput = sh(script: command, returnStdout: true).trim()
-
-    echo "Trivy Scan Results:"
-    echo trivyOutput
-}
