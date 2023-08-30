5754483f6acb90391eec6fc113c3d6214de573a0
Aswin1118 <aswinvj390@gmail.com>
2023-08-30 15:30:01 +0530

    update

--- vars/hadolint.groovy
+++ vars/hadolint.groovy
@@ -3,16 +3,4 @@ def call() {
     def hadolintCommand = "hadolint --ignore DL3008 --ignore DL3015 --ignore DL3047 ${dockerfilePath}"
     def dockerfileExists = fileExists(dockerfilePath)
 
-    if (dockerfileExists) {
-        def lintProcess = hadolintCommand.execute()
-        lintProcess.waitFor()
-
-        if (lintProcess.exitValue() != 0) {
-            throw new RuntimeException("Lint errors found in the Dockerfile")
-        } else {
-            println("No lint errors found.")
-        }
-    } else {
-        throw new RuntimeException("Dockerfile not found.")
-    }
 }
