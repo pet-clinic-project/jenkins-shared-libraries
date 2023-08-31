47d6f8a761126438c5da4339f8172b20921e1a07
Aswin1118 <aswinvj390@gmail.com>
2023-08-31 15:24:50 +0530

    updated script

--- vars/slimImage.groovy
+++ vars/slimImage.groovy
@@ -1,4 +1,6 @@
 def slimBuild(Map imageInfo) {
     def command = "docker-slim build --http-probe=false ${imageInfo.imageName}:${imageInfo.tag}"
     def process = command.execute()
+
+    System.console()?.readLine("Press Enter to continue...")
 }
