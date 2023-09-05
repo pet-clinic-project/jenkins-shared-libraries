2036280a3e73d148d96e14455d49493224ab6662
Aswin1118 <aswinvj390@gmail.com>
2023-09-05 15:56:36 +0530

    updated script

--- vars/hadoLint.groovy
+++ vars/hadoLint.groovy
@@ -3,10 +3,14 @@ import globalVariables
 def call() {
     def dockerFilePath = 'Dockerfile'
     def hadolintCommand = "docker run --rm -i hadolint/hadolint:${globalVariables.hadolintVersion} < ${dockerFilePath}"
+    echo "Executing Hadolint command: ${hadolintCommand}" // Add this line for debugging
 
     def hadolintOutput = sh(script: hadolintCommand, returnStatus: true)
 
     echo "Hadolint Output: ${hadolintOutput}"
 
-
+    if (hadolintOutput != 0) {
+        error("Hadolint failed with exit code ${hadolintOutput}")
+    }
 }
+
