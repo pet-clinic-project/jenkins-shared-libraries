3474d75bd13d4aae7be87384416d0df0c9f05e55
Aswin1118 <aswinvj390@gmail.com>
2023-09-05 12:34:42 +0530

    updated script

--- vars/hadoLint.groovy
+++ vars/hadoLint.groovy
@@ -1,5 +1,10 @@
 def call() {
     def dockerFilePath = 'Dockerfile'
-    def cmd = "docker run --rm -i hadolint/hadolint:v2.7.0 < ${dockerFilePath}"
+    def hadolintCommand = "docker run --rm -i hadolint/hadolint:v2.7.0 < ${dockerFilePath}"
+
+    def hadolintOutput = sh(script: hadolintCommand, returnStatus: true)
+
+    echo "Hadolint Output: ${hadolintOutput}"
+
 
 }
