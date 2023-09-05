0dd131734c44d3d7265dbf5349853fe91d9be1c0
Aswin1118 <aswinvj390@gmail.com>
2023-09-05 12:20:12 +0530

    update

--- vars/hadolint.groovy
+++
@@ -1,6 +0,0 @@
-def call() {
-    def dockerFilePath = 'Dockerfile'
-    def hadolintCommand = "hadolint ${dockerFilePath}"
-    def cmd = "docker run --rm -i hadolint/hadolint:v2.7.0 < ${dockerFilePath}"
-
-}
