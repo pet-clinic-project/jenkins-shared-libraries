963ccd2729010590b44bcaf3fe5320085d952da7
Aswin1118 <aswinvj390@gmail.com>
2023-09-09 16:01:05 +0530

    updated script

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,7 +1,6 @@
 def call(String imageNameAndTag, String htmlTemplate) {
-    def htmlTemplate = htmlTemplate.replace("'", "\\'")
 
-    def command = "trivy image --format template --template '{{${htmlTemplate}}}' -o report.html ${imageNameAndTag}"
+    def command = "trivy image --format template --template \"${htmlTemplate}\" -o report.html ${imageNameAndTag}"
     def trivyOutput = sh(script: command, returnStdout: true).trim()
 
     echo "Trivy Scan Results:"
