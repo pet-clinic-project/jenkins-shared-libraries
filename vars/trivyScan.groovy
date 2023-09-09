77d96f742bf80ec08a4d3d6dc3412a6de1c6043d
Aswin1118 <aswinvj390@gmail.com>
2023-09-09 12:45:48 +0530

    updated script

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,7 +1,12 @@
 def call(String imageNameAndTag) {
-    def command = "trivy image ${imageNameAndTag}"
+    def templateContent = libraryResource('trivy/html.tpl')
+    def command = "trivy image --format template --template '@html.tpl' -o report.html ${imageNameAndTag}"
     def trivyOutput = sh(script: command, returnStdout: true).trim()
 
     echo "Trivy Scan Results:"
     echo trivyOutput
+}
+
+def libraryResource(String resourcePath) {
+    return libraryResourceLoader.fromClassLoader(getClass().classLoader).loadResource(resourcePath).text
 }
