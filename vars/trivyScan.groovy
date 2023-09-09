ad8725a4c1d22b04211315cfa456ec64a0126056
Aswin1118 <aswinvj390@gmail.com>
2023-09-09 13:10:09 +0530

    updated script

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,12 +1,10 @@
 def call(String imageNameAndTag) {
-    def templateContent = libraryResource('trivy/html.tpl')
+    def resourcePath = "${JENKINS_HOME}/workspace/${JOB_NAME}@libs/resources/trivy/html.tpl"
+    def templateContent = readFileFromWorkspace(resourcePath)
+
     def command = "trivy image --format template --template '@html.tpl' -o report.html ${imageNameAndTag}"
     def trivyOutput = sh(script: command, returnStdout: true).trim()
 
     echo "Trivy Scan Results:"
     echo trivyOutput
-}
-
-def libraryResource(String resourcePath) {
-    return libraryResourceLoader.fromClassLoader(getClass().classLoader).loadResource(resourcePath).text
 }
