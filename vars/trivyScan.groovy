ad880afc9347d6ab6da66236217c05e4d7a4a42b
Aswin1118 <aswinvj390@gmail.com>
2023-09-12 10:42:47 +0530

    updated file

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,11 +1,7 @@
 def call(String imageNameAndTag) {
     def htmlTemplate = libraryResource("trivy/html.tpl")
 
-    def templateFile = file("~/workspace/${JOB_NAME}/temp_template.html")
-    templateFile.text = htmlTemplate
-
-
-    def command = "trivy image --format template --template '@${templateFile}' -o report.html ${imageNameAndTag}"
+    def command = "trivy image --format template --template '@${htmlTemplate}' -o report.html ${imageNameAndTag}"
     def trivyOutput = sh(script: command, returnStdout: true).trim()
 
     echo "Trivy Scan Results:"
