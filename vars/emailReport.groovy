391281a4f263ec42c4007c9b42f75846cc2f8aa0
Aswin1118 <aswinvj390@gmail.com>
2023-09-12 13:53:52 +0530

    updated script

--- vars/emailReport.groovy
+++ vars/emailReport.groovy
@@ -1,13 +1,9 @@
-def emailReport(reportPath, imageNameAndTag) {
-  // Get the email address of the recipient.
-    def recipient = "aswin@crunchops.com"
-    def reportPath = "/home/ubuntu/trivy-report.html"
-  // Create a new email message.
+def emailReport(reportPath, imageNameAndTag ,recipient) {
     def email = emailext(
         subject: "Trivy Scan Report",
         body: "Attached is the Trivy scan report for ${imageNameAndTag}.",
-        to: recipient,
-        attach: reportPath
+        to: "${recipient}",
+        attach: "${reportPath}"
     )
 
   // Send the email.
