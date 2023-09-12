fd72656d9f1e383b8aa10ca6d5199c41dc05f9ac
Aswin1118 <aswinvj390@gmail.com>
2023-09-12 15:44:38 +0530

    updated script

--- vars/emailReport.groovy
+++ vars/emailReport.groovy
@@ -1,5 +1,5 @@
 def call(String reportPath, String imageNameAndTag ,String recipient) {
-    def email = emailext(
+    def email = emailPublisher(
         subject: "Trivy Scan Report",
         body: "Attached is the Trivy scan report for ${imageNameAndTag}.",
         to: "${recipient}",
