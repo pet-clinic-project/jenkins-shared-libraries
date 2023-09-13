0865db628a0cc1fe83cc238a824cfd729c133287
Aswin1118 <aswinvj390@gmail.com>
2023-09-13 16:31:41 +0530

    Updated script

--- vars/emailReport.groovy
+++ vars/emailReport.groovy
@@ -1,12 +1,8 @@
 def call(String reportPath, String imageNameAndTag ,String recipient) {
+    def trivyNotification = libraryResource 'notification/tivyNotify.tpl'
     def email = emailext(
         subject: "${PROJECT_NAME} - ${BUILD_NUMBER}",
-        body:  """<html><body>
-                    <p>Click <a href="${BUILD_URL}">here</a> to view the build details.</p>
-                    <pre>
-                    ${readFile(reportPath)}
-                    </pre>
-                </body></html>""",
+        body:  "${trivyNotification}",
         to: "${recipient}",
     )
 }
