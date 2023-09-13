d307a53968a090c00a07984ebb29ed99f915d82c
Aswin1118 <aswinvj390@gmail.com>
2023-09-13 14:36:57 +0530

    updated script

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -3,10 +3,10 @@ def sendEmailNotification(String pipelineStatus, String recipientEmail) {
 
     if (pipelineStatus == 'success') {
         subject = "Terraform Pipeline Success"
-        body = "The Terraform pipeline has successfully completed."
+        body = "The Terraform pipeline has successfully completed. ${BUILD_LOG}"
     } else if (pipelineStatus == 'failure') {
         subject = "Terraform Pipeline Failed"
-        body = "The Terraform pipeline has failed. Please investigate."
+        body = "The Terraform pipeline has failed. Please investigate.${BUILD_LOG}"
     } else {
         subject = "Terraform Pipeline Status: $pipelineStatus"
         body = "The Terraform pipeline is in an unknown status: $pipelineStatus"
