a3a08f71503721762f18193c2f18efec10e0a230
Aswin1118 <aswinvj390@gmail.com>
2023-09-13 10:38:10 +0530

    updated script

--- vars/emailReport.groovy
+++ vars/emailReport.groovy
@@ -3,16 +3,6 @@ def call(String reportPath, String imageNameAndTag ,String recipient) {
         subject: "${PROJECT_NAME} - ${BUILD_NUMBER}",
         body:  """<html><body>
                     <p>Click <a href="${BUILD_URL}">here</a> to view the build details.</p>
-                    <table border="1">
-                        <tr>
-                            <th>Build Number</th>
-                            <td>${BUILD_NUMBER}</td>
-                        </tr>
-                        <tr>
-                            <th>Build URL</th>
-                            <td><a href="${BUILD_URL}">${BUILD_URL}</a></td>
-                        </tr>
-                    </table>
                     <pre>
                     ${readFile(reportPath)}
                     </pre>
