541a54e5b0c8ed0acf4c6e8fefa2a7e1716dffbb
Aswin1118 <aswinvj390@gmail.com>
2023-09-14 12:21:04 +0530

    updated script

--- vars/checkovDockerScan.groovy
+++ vars/checkovDockerScan.groovy
@@ -6,15 +6,15 @@ def call(Map params) {
     def checkovPolicyDir = "${env.WORKSPACE}/custom_policy"
     sh "mkdir -p $checkovPolicyDir"
     // Load the Python policy files from the shared library resources
-    def securityGroupPolicyContent = libraryResource('checkov_policy/SecurityGroupInboundCIDR.py').trim()
+    def dockerPolicyContent = libraryResource('checkov_policy/DockerLatestTag.py').trim()
     def initPolicyContent = libraryResource('checkov_policy/__init__.py').trim()
     // Write the policy contents to files in your workspace
-    writeFile file: "${checkovPolicyDir}/security_group_policy.py", text: securityGroupPolicyContent
+    writeFile file: "${checkovPolicyDir}/DockerLatestTag.py", text: dockerPolicyContent
     writeFile file: "${checkovPolicyDir}/__init__.py", text: initPolicyContent
     checkovScan(projectDirectory, planFileJson, customPolicy, checkovPolicyDir)
 }
 def checkovScan(project_dir, plan_file_json, custom_policy, checkov_policy_dir) {
-    def checkovScanCommand = "checkov -f $project_dir/$plan_file_json --external-checks-dir $checkov_policy_dir --check $custom_policy --hard-fail-on $custom_policy > checkov-report.html"
+    def checkovScanCommand = "checkov -d . --external-checks-dir $checkov_policy_dir --check $custom_policy --hard-fail-on $custom_policy > checkov-report.html"
     sh checkovScanCommand
 }
 
