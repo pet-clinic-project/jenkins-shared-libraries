2b568d37183989e80619452c61d8d97bd2938c0a
Aswin1118 <aswinvj390@gmail.com>
2023-09-15 12:23:54 +0530

    updated script

--- vars/hadoLint.groovy
+++ vars/hadoLint.groovy
@@ -2,16 +2,20 @@ def call() {
     def dockerFilePath = 'Dockerfile'
     def hadolintCommand = "hadolint ${dockerFilePath}"
 
-    def hadolintOutput = sh(script: hadolintCommand, returnStatus: true)
+    // Run the hadolint command and capture both exit status and output
+    def hadolintOutput = sh(script: hadolintCommand, returnStatus: true, returnStdout: true)
 
-    echo "Hadolint Output: ${hadolintOutput}"
+    // Check if there are any warning messages in the hadolint output
+    def hasWarnings = hadolintOutput.contains("warning")
 
-    if (hadolintExitStatus == 0) {
-        echo "Hadolint successfully passed."
+    // Print the hadolint output for reference
+    echo "Hadolint Output:"
+    echo hadolintOutput
+
+    // Determine whether to fail the stage based on the presence of warnings
+    if (hasWarnings) {
+        error("Hadolint detected warnings. Failing the stage.")
     } else {
-        echo "Hadolint returned a non-zero exit status: ${hadolintExitStatus}"
-        // You can add additional actions or error handling here if needed
+        echo "No warnings detected. Continuing with the stage."
     }
-
-
 }
