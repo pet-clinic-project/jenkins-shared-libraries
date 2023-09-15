784118b3b9435ac23112e401404517fb8194325f
Aswin1118 <aswinvj390@gmail.com>
2023-09-15 12:31:27 +0530

    updated script

--- vars/hadoLint.groovy
+++ vars/hadoLint.groovy
@@ -1,21 +1,34 @@
 def call() {
     def dockerFilePath = 'Dockerfile'
-    def hadolintCommand = "hadolint --ignore DL3008 ${dockerFilePath}"
+    def hadolintCommand = "hadolint ${dockerFilePath}"
 
-    // Run the hadolint command and capture both exit status and output
     def hadolintOutput = sh(script: hadolintCommand, returnStatus: true)
 
-    // Check if there are any warning messages in the hadolint output
-    def hasWarnings = hadolintOutput.contains("warning")
+    // Split the hadolint output into lines
+    def lines = hadolintOutput.trim().split('\n')
 
-    // Print the hadolint output for reference
-    echo "Hadolint Output:"
-    echo hadolintOutput
+    // Initialize a flag to check if any warnings were found
+    def hasWarnings = true
 
-    // Determine whether to fail the stage based on the presence of warnings
+    // Iterate through each line of the hadolint output
+    for (def line : lines) {
+        // Check if the line contains a warning
+        if (line =~ /DL\d+ \[\d+mwarning/) {
+            // Print the warning
+            echo "Hadolint Warning: ${line}"
+            
+            // Set the flag to true to indicate that a warning was found
+            hasWarnings = true
+        } else if (line =~ /DL\d+ \[\d+minfo/) {
+            // Print the info note (optional)
+            echo "Hadolint Info: ${line}"
+        }
+    }
+
+    // Check if any warnings were found and fail the stage if so
     if (hasWarnings) {
-        error("Hadolint detected warnings. Failing the stage.")
+        error "Hadolint found warnings. Failing the stage."
     } else {
-        echo "No warnings detected. Continuing with the stage."
+        echo "Hadolint passed without warnings."
     }
 }
