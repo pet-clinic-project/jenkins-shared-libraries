832d1760bb5b5bddd60d393c954498cfe6e07917
Aswin1118 <aswinvj390@gmail.com>
2023-09-15 12:35:02 +0530

    updated script

--- vars/hadoLint.groovy
+++ vars/hadoLint.groovy
@@ -1,34 +1,17 @@
 def call() {
     def dockerFilePath = 'Dockerfile'
-    def hadolintCommand = "hadolint --ignore DL3008 ${dockerFilePath}"
+    def hadolintCommand = "hadolint --ignore DL3008 --ignore DL3015 --ignore DL3047 ${dockerFilePath}"
 
     def hadolintOutput = sh(script: hadolintCommand, returnStatus: true)
 
-    // Split the hadolint output into lines
-    def lines = hadolintOutput.trim().split('\n')
+    echo "Hadolint Output: ${hadolintOutput}"
 
-    // Initialize a flag to check if any warnings were found
-    def hasWarnings = true
-
-    // Iterate through each line of the hadolint output
-    for (def line : lines) {
-        // Check if the line contains a warning
-        if (line =~ /DL\d+ \[\d+mwarning/) {
-            // Print the warning
-            echo "Hadolint Warning: ${line}"
-            
-            // Set the flag to true to indicate that a warning was found
-            hasWarnings = true
-        } else if (line =~ /DL\d+ \[\d+minfo/) {
-            // Print the info note (optional)
-            echo "Hadolint Info: ${line}"
-        }
-    }
-
-    // Check if any warnings were found and fail the stage if so
-    if (hasWarnings) {
-        error "Hadolint found warnings. Failing the stage."
+    if (hadolintExitStatus == 0) {
+        echo "Hadolint successfully passed."
     } else {
-        echo "Hadolint passed without warnings."
+        echo "Hadolint returned a non-zero exit status: ${hadolintExitStatus}"
+        // You can add additional actions or error handling here if needed
     }
+
+
 }
