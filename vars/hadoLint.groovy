5c7d297d16831668717b73b5a21048c066caee31
Aswin1118 <aswinvj390@gmail.com>
2023-09-15 12:32:37 +0530

    updated script

--- vars/hadoLint.groovy
+++ vars/hadoLint.groovy
@@ -1,6 +1,6 @@
 def call() {
     def dockerFilePath = 'Dockerfile'
-    def hadolintCommand = "hadolint ${dockerFilePath}"
+    def hadolintCommand = "hadolint --ignore DL3008 ${dockerFilePath}"
 
     def hadolintOutput = sh(script: hadolintCommand, returnStatus: true)
 
