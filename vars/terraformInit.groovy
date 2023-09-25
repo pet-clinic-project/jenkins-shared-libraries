bc60e96c9f608437e8ee2d43b3a30f601290a445
Administrator <admin@example.com>
2023-09-25 12:21:57 +0530

    Update

--- vars/terraformInit.groovy
+++ vars/terraformInit.groovy
@@ -1,12 +1,20 @@
 def call(Map params) {
    def projectDirectory = params.projectDirectory
+   def variableFile = params.variableFile
 
-   terraformInit(projectDirectory)
+   terraformInit(projectDirectory, variableFile)
 }
 
-def terraformInit(directory) {
-   dir(directory) {
-      def terraformInitCommand = "terraform init --reconfigure"
-      sh terraformInitCommand 
-   }   
+def terraformInit(project_dir, var_file) {
+   dir(project_dir, var_file) {
+      def terraformInitCommand = """
+         terraform init \
+         -backend-config="key=dev/vpc.tfstate" \
+         -backend-config="bucket=dcube-terraform-state" \
+         -backend-config="region=us-west-2" \
+         -backend-config="dynamodb_table=terraform-state-lock" \
+         -var-file=../../vars/infra/dev/${var_file}
+      """
+      sh terraformInitCommand.trim()
+   }
 }
