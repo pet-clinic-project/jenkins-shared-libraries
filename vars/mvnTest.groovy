652d1c7f094591c43f69e5ad17f264741ff1ec7e
Aswin1118 <aswinvj390@gmail.com>
2023-09-27 12:04:03 +0530

    updated script

--- vars/mvnTest.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-    def mvnCommand = './mvnw test'
-    def exitCode = sh(script: mvnCommand, returnStatus: true)
-
-    if (exitCode != 0) {
-        error "Maven test failed with exit code: $exitCode"
-    }
-}
