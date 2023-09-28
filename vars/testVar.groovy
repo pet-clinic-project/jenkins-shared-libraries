9a9e19d4e588c13ea8177d3c60dee59cff4762f5
Aswin1118 <aswinvj390@gmail.com>
2023-09-28 17:14:44 +0530

    updated script

--- vars/testVar.groovy
+++ vars/testVar.groovy
@@ -1,5 +1,5 @@
 // vars/myVarFunction.groovy
 def call() {
     // Access the global variable from src/GlobalVars.groovy
-    echo "Global variable value: ${GlobalVars.myGlobalVar}"
+    echo "Global variable value: ${GlobalVar.myGlobalVar}"
 }
