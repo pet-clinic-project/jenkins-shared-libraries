556521c42671cdc37a3eff6dc34c442e6f563e31
Aswin1118 <aswinvj390@gmail.com>
2023-09-29 16:05:25 +0530

    updated script

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,10 +1,12 @@
-def call(String imageNameAndTag) {
+import org.techiescamp.GlobalConfig
+
+def call(String imageName) {
     script {
                     def tplContent = libraryResource "trivy/html.tpl"
                     writeFile file: "${WORKSPACE}/html.tpl", text: tplContent
                 }
 
-    def command = "trivy image --exit-code 1 --severity CRITICAL --cache-dir /tmp/trivy-cache --format template --template '@${WORKSPACE}/html.tpl' -o ${WORKSPACE}/trivy-report.html ${imageNameAndTag}"
+    def command = "trivy image --exit-code 1 --severity CRITICAL --cache-dir /tmp/trivy-cache --format template --template '@${WORKSPACE}/html.tpl' -o ${WORKSPACE}/trivy-report.html ${imageName}:${GlobalConfig.versionTag}.${BUILD_NUMBER}"
     def trivyOutput = sh(script: command, returnStdout: true).trim()
 
     echo "Trivy Scan Results:"
