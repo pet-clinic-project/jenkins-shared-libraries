9fc7b2826712756e77d5c357ae20c940a4d0746a
Aswin1118 <aswinvj390@gmail.com>
2023-09-29 18:00:51 +0530

    updated script

--- vars/hadoLint.groovy
+++ vars/hadoLint.groovy
@@ -1,8 +1,7 @@
 def call() {
-    def dockerFilePath = 'Dockerfile'
     def hadolintConfigFile = libraryResource('hadolint/.hadolint.yaml')
 
-    def hadolintCommand = "hadolint --config ${hadolintConfigFile} ${dockerFilePath}"
+    def hadolintCommand = "hadolint --config ${hadolintConfigFile} Dockerfile"
 
     def hadolintOutput = sh(script: hadolintCommand, returnStatus: true)
 
