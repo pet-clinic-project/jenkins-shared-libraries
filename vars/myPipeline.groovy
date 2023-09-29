66e32c2edb1843de2ac498e8aec33a689da70691
Aswin1118 <aswinvj390@gmail.com>
2023-09-29 13:04:01 +0530

    updated script

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,7 +1,7 @@
 import org.techiescamp.GlobalConfig
 
 def call() {
-    echo "Using global variable: ${GlobalVar.versionTag}"
+    echo "Using global variable: ${GlobalConfig.versionTag}"
     echo "Using global variable: ${EndPoint.sonarEndPoint}"
     echo "Using global variable: ${EndPoint.nexusEndPoint}"
     echo "Using global variable: ${AwsConfig.awsRegion}"
