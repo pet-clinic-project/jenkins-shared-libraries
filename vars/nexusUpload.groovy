a1d6482db209496cdc579abbdaff31f4a699f206
Aswin1118 <aswinvj390@gmail.com>
2023-09-30 12:28:18 +0530

    updated script

--- vars/nexusUpload.groovy
+++ vars/nexusUpload.groovy
@@ -2,7 +2,7 @@ def call(String nexusRepository, String nexusCredentialsId, String jarFileName) 
     
     withCredentials([usernamePassword(credentialsId: nexusCredentialsId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
     
-        def jarFile = findFiles(glob: 'target/*.jar').first()
+        def jarFile = "${WORKSPACE}/target/*.jar"
 
         def uploadCommand = "curl -v -u ${USERNAME}:${PASSWORD} --upload-file ${jarFile} ${GlobalConfig.nexusEndPoint}/${nexusRepository}/${jarFile.name}"
         def exitCode = sh(script: uploadCommand, returnStatus: true)
