ff09a62024cb9408096a112f4376c149dfe77239
Aswin1118 <aswinvj390@gmail.com>
2023-09-30 12:22:03 +0530

    updated script

--- vars/nexusUpload.groovy
+++ vars/nexusUpload.groovy
@@ -1,14 +1,14 @@
-def call(String nexusUrl, String nexusRepository, String nexusCredentialsId, String jarFileName) {
-    def credentials = credentials(nexusCredentialsId)
-    def nexusUsername = credentials.Username
-    def nexusPassword = credentials.Password
+def call(String nexusRepository, String nexusCredentialsId, String jarFileName) {
+    
+    withCredentials([usernamePassword(credentialsId: nexusCredentialsId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+    
+        def jarFile = findFiles(glob: 'target/*.jar').first()
 
-    def jarFile = findFiles(glob: 'target/*.jar').first()
+        def uploadCommand = "curl -v -u ${USERNAME}:${PASSWORD} --upload-file ${jarFile} ${GlobalConfig.nexusEndPoint}/${nexusRepository}/${jarFile.name}"
+        def exitCode = sh(script: uploadCommand, returnStatus: true)
 
-    def uploadCommand = "curl -v -u ${nexusUsername}:${nexusPassword} --upload-file ${jarFile} ${nexusUrl}/${nexusRepository}/${jarFile.name}"
-    def exitCode = sh(script: uploadCommand, returnStatus: true)
-
-    if (exitCode != 0) {
-        error "Failed to upload JAR file to Nexus. Exit code: ${exitCode}"
+        if (exitCode != 0) {
+            error "Failed to upload JAR file to Nexus. Exit code: ${exitCode}"
+        }
     }
 }
