aa7e391ba2e80a991df55323a5818a9fb95bcc0a
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-10-05 18:44:31 +0530

    Update terraformDestroy.groovy

--- vars/terraformDestroy.groovy
+++ vars/terraformDestroy.groovy
@@ -1,21 +1,13 @@
 def call(Map params) {
    def projectDirectory = params.projectDirectory
-   def tfstateFile = params.tfstateFile
    def variableFile = params.variableFile
 
-   terraformDestroy(projectDirectory, tfstateFile, variableFile)
+   terraformDestroy(projectDirectory, variableFile)
 }
 
-def terraformDestroy(project_dir, tfstateFile, var_file) {
+def terraformDestroy(project_dir, var_file) {
    dir(project_dir) {
-      def terraformDestroyCommand = """
-        terraform destroy \\
-          -backend-config="key=dev/${tfstateFile}" \\
-          -backend-config="bucket=dcube-terraform-state" \\
-          -backend-config="region=us-west-2" \\
-          -backend-config="dynamodb_table=terraform-state-lock" \\
-          -var-file=../../vars/infra/dev/${var_file} --auto-approve
-      """
+      def terraformDestroyCommand = "terraform destroy -var-file=../../vars/infra/dev/${var_file} --auto-approve"
 
       sh terraformDestroyCommand
    }
