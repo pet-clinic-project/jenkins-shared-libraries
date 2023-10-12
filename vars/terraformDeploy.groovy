e39835c7481a849af5f32fb34623a0d134bb6ba0
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-10-12 19:28:05 +0530

    Update terraformDeploy.groovy

--- vars/terraformDeploy.groovy
+++ vars/terraformDeploy.groovy
@@ -5,18 +5,14 @@ def init(Map params) {
    def tfstateFile = params.tfstateFile
    def variableFile = params.variableFile
 
-   terraformInit(projectDirectory, tfstateFile, variableFile)
-}
-
-def terraformInit(project_dir, tfstateFile, var_file) {
-   dir(project_dir) {
+   dir(projectDirectory) {
       def terraformInitCommand = """
         terraform init \\
           -backend-config="key=dev/${tfstateFile}" \\
           -backend-config="bucket=${GlobalConfig.backendS3Bucket}" \\
           -backend-config="region=${GlobalConfig.awsRegion}" \\
           -backend-config="dynamodb_table=${GlobalConfig.backendDynamodbTable}" \\
-          -var-file=../../vars/infra/stage/${var_file}
+          -var-file=../../vars/infra/stage/${variableFile}
       """
 
       sh terraformInitCommand
@@ -26,7 +22,7 @@ def terraformInit(project_dir, tfstateFile, var_file) {
 def validate(Map params) {
    def projectDirectory = params.projectDirectory
 
-   dir(directory) {
+   dir(projectDirectory) {
       def terraformValidateCommand = "terraform validate"
       sh terraformValidateCommand 
    }   
