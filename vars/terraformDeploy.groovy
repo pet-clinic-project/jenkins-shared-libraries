5c095a7fcaccf38d7f162a138ef8426a75ecb05a
Administrator <admin@example.com>
2023-10-12 19:43:47 +0530

    update

--- vars/terraformDeploy.groovy
+++ vars/terraformDeploy.groovy
@@ -1,70 +1,75 @@
 import org.techiescamp.GlobalConfig
 
-def call(Map params) {
+def init(Map params) {
    def projectDirectory = params.projectDirectory
-   def variableFile = params.variableFile
    def tfstateFile = params.tfstateFile
-   def planFile = params.planFile
-   def redirectPlanFile = params.redirectPlanFile
-   def amiId = params.amiId
+   def variableFile = params.variableFile
 
-   terraformInit(projectDirectory, tfstateFile, variableFile)
-   terraformValidate(projectDirectory)
-   terraformPlan(projectDirectory, variableFile, planFile, amiId)
-   terraformShow(projectDirectory, planFile, redirectPlanFile)
-   terraformApply(projectDirectory, variableFile, amiId)
-   terraformDestroy(projectDirectory, variableFile, amiId)
-}
-
-def terraformInit(project_dir, tfstateFile, var_file) {
-   dir(project_dir) {
+   dir(projectDirectory) {
       def terraformInitCommand = """
         terraform init \\
           -backend-config="key=dev/${tfstateFile}" \\
           -backend-config="bucket=${GlobalConfig.backendS3Bucket}" \\
           -backend-config="region=${GlobalConfig.awsRegion}" \\
           -backend-config="dynamodb_table=${GlobalConfig.backendDynamodbTable}" \\
-          -var-file=../../vars/infra/stage/${var_file}
+          -var-file=../../vars/infra/stage/${variableFile}
       """
 
       sh terraformInitCommand
    }
 }
 
-def terraformValidate(directory) {
-   dir(directory) {
+def validate(Map params) {
+   def projectDirectory = params.projectDirectory
+
+   dir(projectDirectory) {
       def terraformValidateCommand = "terraform validate"
       sh terraformValidateCommand 
    }   
 }
 
-def terraformPlan(project_dir, var_file, plan_file, ami_id) {
-   dir(project_dir) {
+def plan(Map params) {
+   def projectDirectory = params.projectDirectory
+   def variableFile = params.variableFile
+   def planFile = params.planFile
+   def amiId = params.amiId
+
+   dir(projectDirectory) {
      
-      def terraformPlanCommand = "terraform plan -var=ami_id=$ami_id -var-file=../../vars/$var_file -out $plan_file"
+      def terraformPlanCommand = "terraform plan -var=ami_id=$amiId -var-file=../../vars/$variableFile -out $planFile"
       sh terraformPlanCommand
    }   
 }
 
-def terraformShow(project_dir, plan_file, redir_plan_file) {
-   dir(project_dir) {
-      def terraformShowCommand = "terraform show -json $plan_file | jq > $redir_plan_file"
+def show(Map params) {
+   def projectDirectory = params.projectDirectory
+   def planFile = params.planFile
+   def redirectPlanFile = params.redirectPlanFile
+
+   dir(projectDirectory) {
+      def terraformShowCommand = "terraform show -json $planFile | jq > $redirectPlanFile"
       sh terraformShowCommand
    }   
 }
 
-def terraformApply(project_dir, var_file, ami_id) {
-   dir(project_dir) {
-      def terraformApplyCommand = "terraform apply -var=ami_id=$ami_id -var-file=../../vars/$var_file --auto-approve"
+def apply(Map params) {
+   def projectDirectory = params.projectDirectory
+   def variableFile = params.variableFile
+   def amiId = params.amiId
+
+   dir(projectDirectory) {
+      def terraformApplyCommand = "terraform apply -var=ami_id=$amiId -var-file=../../vars/$variableFile --auto-approve"
       sh terraformApplyCommand 
    }   
 }
 
-def terraformDestroy(project_dir, var_file, ami_id) {
-   dir(project_dir) {
-      def terraformDestroyCommand = "terraform destroy -var=ami_id=$ami_id -var-file=../../vars/${var_file} --auto-approve"
+def destroy(Map params) {
+   def projectDirectory = params.projectDirectory
+   def variableFile = params.variableFile
+   def amiId = params.amiId
 
-      sh terraformDestroyCommand
-   }
+   dir(projectDirectory) {
+      def terraformDestroyCommand = "terraform destroy -var=ami_id=$amiId -var-file=../../vars/$variableFile --auto-approve"
+      sh terraformDestroyCommand 
+   }   
 }
-
