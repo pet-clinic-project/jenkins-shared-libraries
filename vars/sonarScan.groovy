56c6e83b447207a43e72a65d553874ee4077f3aa
Aswin1118 <aswinvj390@gmail.com>
2023-10-12 14:58:40 +0530

    updated script

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -14,8 +14,36 @@ def call(String projectKey, String organization, String sonarTokenCredentialId) 
     """
 
     def status = sh(script: sonarCommand, returnStatus: true)
-    
+
     if (status != 0) {
         error "SonarQube scan failed with exit code: ${status}"
+    } else {
+        // Retrieve scan report using SonarCloud API
+        def apiUrl = "${GlobalConfig.sonarEndPoint}/api/measures/component"
+        def analysisId = sh(script: 'mvn sonar:sonar -Dsonar.showProfiling=true | grep "ANALYSIS SUCCESSFUL, you can browse " | cut -d\' \' -f10', returnStdout: true).trim()
+
+        def apiParams = [
+            component: projectKey,
+            metricKeys: 'code_smells,bugs,vulnerabilities,coverage,ncloc,violations',
+            additionalParams: [
+                'metricKeys': 'code_smells,bugs,vulnerabilities,coverage,ncloc,violations'
+            ]
+        ]
+
+        def apiResponse = httpRequest(
+            acceptType: 'APPLICATION_JSON',
+            contentType: 'APPLICATION_JSON',
+            httpMode: 'GET',
+            timeout: 30,
+            url: apiUrl,
+            authentication: sonarToken,
+            customHeaders: [[name: 'Authorization', value: "Bearer ${sonarToken}"]],
+            ignoreSslErrors: true,
+            validResponseCodes: '200',
+            responseHandle: 'NONE',
+            requestBody: groovy.json.JsonOutput.toJson(apiParams)
+        )
+
+        echo "SonarQube scan report: ${apiResponse}"
     }
 }
