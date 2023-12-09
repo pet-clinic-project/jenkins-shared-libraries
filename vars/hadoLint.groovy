5fafb245ce6777727fbbabc45955773a063c8c17
Aswin1118 <aswinvj390@gmail.com>
2023-12-09 15:22:36 +0530

    updated file

--- vars/hadoLint.groovy
+++ vars/hadoLint.groovy
@@ -8,12 +8,4 @@ def call() {
                 }
 
     def hadolintCommand = "hadolint --config ${WORKSPACE}/.hadolint.yaml ${dockerFilePath}"
-
-    def hadolintOutput = sh(script: hadolintCommand, returnStatus: true)
-
-    echo "Hadolint Exit Code: ${hadolintOutput}"
-
-    if (hadolintOutput != 0) {
-        error "Hadolint failed with exit code ${hadolintOutput}"
-    }
 }
