e0b23cca2bf16ef45fa6842edf6117c3936c2473
Aswin1118 <aswinvj390@gmail.com>
2023-12-09 11:18:57 +0530

    added trivy configuration yml file

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -4,9 +4,12 @@ def call(String imageName) {
     script {
                     def tplContent = libraryResource "trivy/html.tpl"
                     writeFile file: "${WORKSPACE}/html.tpl", text: tplContent
+
+                    def trivyConfigContent = libraryResource "trivy/trivy.yml"
+                    writeFile file: "${WORKSPACE}/trivy.yml", text: trivyConfigContent
                 }
 
-    def command = "trivy image --exit-code 1 --severity CRITICAL --cache-dir /tmp/trivy-cache --format template --template '@${WORKSPACE}/html.tpl' -o ${WORKSPACE}/trivy-report.html ${imageName}:${GlobalConfig.versionTag}.${BUILD_NUMBER}"
+    def command = "trivy image --config ${WORKSPACE}/trivy.yml ${imageName}:${GlobalConfig.versionTag}.${BUILD_NUMBER}"
     def trivyOutput = sh(script: command, returnStdout: true).trim()
 
     echo "Trivy Scan Results:"
