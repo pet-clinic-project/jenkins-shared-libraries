0f45a1f4302471d92c2989b92e80fcfe258b1b3b
Aswin1118 <aswinvj390@gmail.com>
2024-08-22 12:56:32 +0530

    updated emailreport script

--- vars/emailReport.groovy
+++ vars/emailReport.groovy
@@ -1,11 +1,8 @@
-def call(String reportPath, String recipient) {
+def call(String recipient) {
     def email = emailext(
         subject: "${JOB_NAME} - ${BUILD_NUMBER}",
         body:  """<html><body>
                     <p>Click <a href="${BUILD_URL}">here</a> to view the build details.</p>
-                    <pre>
-                    ${readFile(reportPath)}
-                    </pre>
                 </body></html>""",
         to: "${recipient}",
     )
