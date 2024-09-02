29833a9a332fac7478f6b13cce487308536ab748
Aswin1118 <aswinvj390@gmail.com>
2024-09-02 12:23:11 +0530

    update

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -30,7 +30,7 @@ def kaniko() {
     try {
         setupTrivyFiles()
 
-        def command = "trivy image --template '@${WORKSPACE}/html.tpl' -o ${WORKSPACE}/trivy-report.html --input ${WORKSPACE}/${BUILD_NUMBER}.tar"
+        def command = "trivy image --format template --template '@${WORKSPACE}/html.tpl' -o ${WORKSPACE}/trivy-report.html --input ${WORKSPACE}/${BUILD_NUMBER}.tar"
         def result = runTrivyCommand(command)
 
         return result.exitCode
