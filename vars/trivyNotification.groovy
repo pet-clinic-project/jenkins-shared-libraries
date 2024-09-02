41ec74e8719a20f54c12ddcacd7fccbb9b2db723
Aswin1118 <aswinvj390@gmail.com>
2024-09-02 16:16:32 +0530

    update

--- vars/trivyNotification.groovy
+++ vars/trivyNotification.groovy
@@ -1,12 +1,20 @@
 def call(String reportPath, String recipient) {
-    emailext(
-        subject: "${JOB_NAME} - ${BUILD_NUMBER}",
-        body: """<html><body>
-                    <p>Click <a href="${BUILD_URL}">here</a> to view the build details.</p>
-                    <p>The scan report is attached to this email.</p>
-                </body></html>""",
-        to: "${recipient}",
-        mimeType: 'text/html',
-        attachmentsPattern: "${reportPath}"
-    )
+    // Check if the file exists
+    if (fileExists(reportPath)) {
+        echo "File found: ${reportPath}"
+
+        emailext(
+            subject: "${JOB_NAME} - ${BUILD_NUMBER}",
+            body: """<html><body>
+                        <p>Click <a href="${BUILD_URL}">here</a> to view the build details.</p>
+                        <p>The scan report is attached to this email.</p>
+                    </body></html>""",
+            to: "${recipient}",
+            mimeType: 'text/html',
+            attachmentsPattern: reportPath
+        )
+    } else {
+        echo "File not found: ${reportPath}"
+        error "Report file not found. Cannot send email with attachment."
+    }
 }
