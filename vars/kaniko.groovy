dd3433c7ae2b8e9ef18b00d123a8df6497b9a0e2
Aswin1118 <aswinvj390@gmail.com>
2024-09-02 20:11:09 +0530

    update

--- vars/kaniko.groovy
+++ vars/kaniko.groovy
@@ -20,31 +20,20 @@ def build() {
     }
 }
 
-def push(String imageName, String imageTag, String credentialsId) {
-    try {
-        withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_HUB_USR', passwordVariable: 'DOCKER_HUB_PSW')]) {
-            sh """
-                mkdir -p /kaniko/.docker
-                echo '{"auths":{"https://index.docker.io/v1/":{"auth":"'"\$(echo -n ${DOCKER_HUB_USR}:${DOCKER_HUB_PSW} | base64)"'"}}}' > /kaniko/.docker/config.json
-            """
-            def dockerPush = """
-                /kaniko/executor \
-                --dockerfile="${WORKSPACE}/Dockerfile" \
-                --context "${WORKSPACE}" \
-                --destination "${imageName}:${imageTag}"
-            """
+def push(Map params) {
+    def imageName = params.imageName
+    def imageTag = params.imageTag
+    def credentialsId = params.credentialsId
 
-            def exitCode = sh(script: dockerPush, returnStatus: true)
-
-            if (exitCode != 0) {
-                error "Kaniko push failed with exit code: ${exitCode}"
-            } else {
-                echo "Kaniko push succeeded."
-            }
-        }
-    } catch (Exception e) {
-        error "Exception during Kaniko push: ${e.getMessage()}"
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+        sh """
+        /kaniko/executor --dockerfile=Dockerfile \
+                         --dockerfile=Dockerfile \
+                         --context `pwd` \
+                         --destination=${imageName}:${imageTag}
+        """
     }
 }
+
 
 
