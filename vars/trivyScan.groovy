8493143181e844f3c27245a5a95ca122ecce54f7
Aswin1118 <aswinvj390@gmail.com>
2024-09-02 12:46:50 +0530

    update

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -26,6 +26,22 @@ def runTrivyCommand(String command) {
     return [exitCode: exitCode, output: output]
 }
 
+def test() {
+    script {
+                    def tplContent = libraryResource "trivy/html.tpl"
+                    writeFile file: "${WORKSPACE}/html.tpl", text: tplContent
+
+                    def trivyConfigContent = libraryResource "trivy/trivy.yml"
+                    writeFile file: "${WORKSPACE}/trivy.yml", text: trivyConfigContent
+                }
+
+    def command = "trivy image --config ${WORKSPACE}/trivy.yml --format template --template '@${WORKSPACE}/html.tpl' -o ${WORKSPACE}/trivy-report.html --input ${WORKSPACE}/${BUILD_NUMBER}.tar"
+    def trivyOutput = sh(script: command, returnStdout: true).trim()
+
+    echo "Trivy Scan Results:"
+    echo trivyOutput
+}
+
 def kaniko() {
     try {
         setupTrivyFiles()
