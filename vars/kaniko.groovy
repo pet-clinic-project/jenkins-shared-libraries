6f6b1b59b909d416e98425ff70a513b4767bc7dd
Aswin1118 <aswinvj390@gmail.com>
2024-09-02 20:59:11 +0530

    update

--- vars/kaniko.groovy
+++ vars/kaniko.groovy
@@ -21,12 +21,12 @@ def build() {
 }
 
 def push(String credentialsId, String imageName, String imageTag) {
-        withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_HUB_USR', passwordVariable: 'DOCKER_HUB_PSW')]) {
-            script {
-                sh """
-                    echo '{"auths":{"https://index.docker.io/v1/":{"auth":"'"\$(echo -n ${DOCKER_HUB_USR}:${DOCKER_HUB_PSW} | base64)"'"}}}' > /kaniko/.docker/config.json
-                    /kaniko/executor --dockerfile="/Dockerfile" --context `pwd` --destination ${imageName}:${imageTag}
-                """
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_HUB_USR', passwordVariable: 'DOCKER_HUB_PSW')]) {
+        script {
+            sh """
+                echo '{"auths":{"https://index.docker.io/v1/":{"auth":"'"\$(echo -n ${DOCKER_HUB_USR}:${DOCKER_HUB_PSW} | base64)"'"}}}' > /kaniko/.docker/config.json
+                /kaniko/executor --dockerfile="/Dockerfile" --context `pwd` --destination ${imageName}:${imageTag}
+            """
         }
     }
 }
