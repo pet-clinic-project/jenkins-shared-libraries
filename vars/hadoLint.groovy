b549d85b2839a42e1ad9b7798204a52c6a104379
Aswin1118 <aswinvj390@gmail.com>
2024-09-03 20:34:58 +0530

    update

--- vars/hadoLint.groovy
+++ vars/hadoLint.groovy
@@ -1,13 +1,11 @@
 def call() {
-
-    def dockerFilePath = "${WORKSPACE}/Dockerfile"
     
     script {
                     def tplContent = libraryResource "hadolint/.hadolint.yaml"
                     writeFile file: "${WORKSPACE}/.hadolint.yaml", text: tplContent
                 }
 
-    def hadolintCommand = "hadolint --config ${WORKSPACE}/.hadolint.yaml ${dockerFilePath}"
+    def hadolintCommand = "hadolint --config ${WORKSPACE}/.hadolint.yaml ${WORKSPACE}/Dockerfile"
 
     def hadolintOutput = sh(script: hadolintCommand, returnStatus: true)
 
