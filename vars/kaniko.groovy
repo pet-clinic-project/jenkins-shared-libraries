0d775c01f8cc604182c8dd17804216263db9dc52
Aswin1118 <aswinvj390@gmail.com>
2024-09-03 10:50:55 +0530

    update

--- vars/kaniko.groovy
+++ vars/kaniko.groovy
@@ -20,13 +20,13 @@ def build() {
     }
 }
 
-def push() {
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_HUB_USR', passwordVariable: 'DOCKER_HUB_PSW')]) {
+def push(String registryName, String imageTag, String registryCred) {
+    withCredentials([usernamePassword(credentialsId: registryCred, usernameVariable: 'DOCKER_HUB_USR', passwordVariable: 'DOCKER_HUB_PSW')]) {
         script {
             try {
                 sh """
                     echo '{"auths":{"https://index.docker.io/v1/":{"auth":"'"\$(echo -n ${DOCKER_HUB_USR}:${DOCKER_HUB_PSW} | base64)"'"}}}' > /kaniko/.docker/config.json
-                    /kaniko/executor --dockerfile="/Dockerfile" --context "." --destination "aswinvj/test:2.0"
+                    /kaniko/executor --dockerfile="/Dockerfile" --context "." --destination "${registryName}/${imageTag}"
                 """
             } catch (Exception e) {
                 echo "Error occurred during Kaniko build and push: ${e.getMessage()}"
